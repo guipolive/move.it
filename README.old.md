2d11732eef06c33f9bae290f9b577e4e483acbcf
Guilherme Oliveira <gui.passosdeoliveira@gmail.com>
2021-02-24 08:32:12 -0300

    recreate project with next.js

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# move.it
-App feito durante a NLW#4
---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+
+import {ExperienceBar} from '../components/ExperienceBar';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet"></link>
+      </Head>
+
+
+      <div className="container" >
+        <ExperienceBar />
+      </div>
+    </>
+  )
+}
